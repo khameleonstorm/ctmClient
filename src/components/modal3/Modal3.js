6342effa22a9f74a89b80da327ce31058cd2a94c
khameleonstorm <khameleonstorm@gmail.com>
2023-06-15 08:49:33 +0100

    p

--- src/components/modal3/Modal3.js
+++ src/components/modal3/Modal3.js
@@ -24,7 +24,7 @@ export default function Modal3({type, handleModal, user}) {
     setError(null)
     setSuccess(null)
     try {
-      const response = await fetch(`http://localhost:5000/api/withdrawals`, {
+      const response = await fetch(`https://ctmserver.herokuapp.com/api/withdrawals`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify( {...withdraw, amount: Number(usd)} )
