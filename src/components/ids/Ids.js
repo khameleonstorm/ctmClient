304da2bc649709c98d151854e591297167a6b11d
khameleonstorm <khameleonstorm@gmail.com>
2023-07-10 06:13:11 +0100

    'm'

--- src/components/ids/Ids.js
+++ src/components/ids/Ids.js
@@ -3,23 +3,18 @@ import s from './Ids.module.css'
 import { ImSpinner8 } from 'react-icons/im'
 
 export default function Ids({nins}) {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [success, setSuccess] = useState(false)
+  const [loadingId, setLoadingId] = useState(null)
   const [email, setEmail] = useState(null)
   const [nin, setNin] = useState(null)
-  const [showModal, setShowModal] = useState(false)
 
-  const handleApproval  = (userEmail, userNin) => {
+  const handleApproval  = (userEmail, userNin, id) => {
     setEmail(userEmail)
     setNin(userNin)
-    setShowModal(true)
+    handleSubmit(id)
   }
 
-  const handleSubmit = async () => {
-    setLoading(true)
-    setError(null)
-    setSuccess(null)
+  const handleSubmit = async (id) => {
+    setLoadingId(id)
     try {
       const res = await fetch(`https://ctmserver.herokuapp.com/api/nins/`, {
         method: 'PUT',
@@ -28,38 +23,25 @@ export default function Ids({nins}) {
       })
 
       const data = await res.json()
-      if (res.status === 200) setSuccess('NIN approved successfully')
+      if (res.status === 200) alert('NIN approved successfully')
       else throw new Error(data.message)
-    } catch (error) { setError(error.message) }
-    setLoading(false)
+    } catch (error) { alert(error.message) }
+    setLoadingId(null)
   }
 
 
   return (
     <div className={s.ctn}>
-      {nins?.map(nin => 
-      <div className={s.card} key={nin._id}>
+      {nins?.map((nin, i) => 
+      <div className={s.card} key={i}>
         <div className={s.left}><p className={s.name}>{nin.name}</p><p>{nin.nin}</p></div>
         <div className={s.right}> 
           {nin.status && <p className={s.status} style={{color: '#00ff00'}}>Approved</p>}
           {!nin.status && <p className={s.status} style={{color: '#ff9900'}}>Pending</p>}
-          <p className='modalBtn' onClick={() => handleApproval(nin.email, nin.nin)}><span>Approve</span></p>
+          <p className='modalBtn' onClick={() => handleApproval(nin.email, nin.nin, i)}>{loadingId === i? <ImSpinner8 />: <span>Approve</span>}</p>
         </div>
         </div>
-          )}
-
-        {showModal &&
-        <div className='modalCtn'>
-          <div className='modalWrp'>
-            <h1 className='modalTitle'>Sure You Want To Approve This NIN?</h1>
-            <p className='cancel' onClick={() => setShowModal(false)}><span>Cancel</span></p> 
-              <p className='modalBtn' onClick={handleSubmit}>{!loading && <span>Yes</span>} {loading && <span><ImSpinner8 className='spin'/></span>}</p>
-              {error && <p className='formError'>{error}</p>}
-              {success && <p className='formSuccess'>{success}</p>}
-          </div>
-        </div>
-        }
-            
+          )}  
     </div>
   )
 }
